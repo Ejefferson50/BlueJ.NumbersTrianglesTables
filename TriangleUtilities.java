7a3fd065e82232ea1875c714685a9be9a8569ec4
Eric Jefferson <ejefferson50@gmail.com>
2019-02-07 17:47:06 -0500

    TriangleUtilities Done

--- TriangleUtilities.java
+++ TriangleUtilities.java
@@ -3,19 +3,51 @@
 public class TriangleUtilities {
 
     public static String getRow(int numberOfStars) {
-        return null;
+        String stars = "";
+        for (int i = 0; i < numberOfStars; i++){
+            stars = stars + "*";
+        }
+        return stars ;
+       
     }
     
     public static String getTriangle(int numberOfRows) {
-        return null;
+        String stars = "";
+        for (int i = 1; i <= numberOfRows; i++)
+        {
+           for (int j = 0; j < i; j++){
+            stars = stars + "*";
+        }
+        stars = stars + "\n";
+    }
+        System.out.println(stars);
+        return stars ;
     }
 
 
     public static String getSmallTriangle() {
-        return null;
+        String stars = "";
+        
+        for (int i = 0; i < 4; i++)
+        {
+            for (int d = 0; d<=i; d++){
+            stars = stars + "*";
+        }
+        stars = stars + "\n";
+    }
+        return stars;
     }
 
     public static String getLargeTriangle() {
-        return null;
+        String stars = "";
+        
+        for (int i = 0; i < 9; i++)
+        {
+            for (int d = 0; d<=i; d++){
+            stars = stars + "*";
+        }
+        stars = stars + "\n";
+    }
+        return stars;
     }
 }
