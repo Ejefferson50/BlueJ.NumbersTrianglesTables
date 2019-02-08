ddf1a070e1212631e9054a98b1019f272cc7b28f
Eric Jefferson <ejefferson50@gmail.com>
2019-02-07 20:01:01 -0500

    Finished Tables

--- TableUtilities.java
+++ TableUtilities.java
@@ -2,14 +2,38 @@
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
-    }
-
+        int size = 5;
+        String small = "";
+        for (int x = 1; x <= size; x++) {
+            for (int y = 1; y <= size; y++){
+                small += small.format("%3d", x * y) + " |";
+            }
+       small += "\n";
+   }
+   return small;
+  
+}
     public static String getLargeMultiplicationTable() {
-        return null;
+        int size = 10;
+        String large = "";
+        for (int x = 1; x <= size; x++) {
+            for (int y = 1; y <= size; y++){
+                large += large.format("%3d", x * y) + " |";
+            }
+       large += "\n";
+   }
+   return large;
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+       
+        String mystery = "";
+        for (int x = 1; x <= tableSize; x++) {
+            for (int y = 1; y <= tableSize; y++){
+                mystery += mystery.format("%3d", x * y) + " |";
+            }
+       mystery += "\n";
+   }
+   return mystery;
     }
 }
